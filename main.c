ea83ad74572c18b830ef7bec41a73a6c8f93a06e
Mike <tankf33der@disroot.org>
2020-05-09 19:22:10 +0300

    .

--- main.c
+++ main.c
@@ -3,14 +3,25 @@
 #include "curve9767.h"
 
 int main(void) {
-    curve9767_point Q;
-    shake_context sc;
+    curve9767_point Q1;
+    shake_context sc1;
     uint8_t mike[4] = "mike";
 
-    shake_init(&sc, 256);
-    shake_inject(&sc, mike, 4);
-    shake_flip(&sc);
-    curve9767_hash_to_curve(&Q, &sc);
+    // hash_to_curve
+    shake_init(&sc1, 256);
+    shake_inject(&sc1, mike, 4);
+    shake_flip(&sc1);
+    curve9767_hash_to_curve(&Q1, &sc1);
+
+    //keygen
+    uint8_t seed[32];
+    uint8_t tmp[64];
+    curve9767_point Q2;
+    curve9767_scalar s2;
+    for (size_t i = 0; i < 32; i++) {
+        seed[i] = i;
+    }
+    curve9767_keygen(&s2, tmp, &Q2, seed, sizeof seed);
 
     return 0;
 }
